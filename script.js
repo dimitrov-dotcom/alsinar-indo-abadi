fb3873e1b499a7621889ca5c3540608b8c8cc8ad
Dimitrov <dimitrov@users.noreply.github.com>
2025-12-26 11:16:38 +0800

    Fix hero slider & fade-up content

--- script.js
+++ script.js
@@ -32,3 +32,12 @@ const observer = new IntersectionObserver(entries => {
 document.querySelectorAll(".fade-up").forEach(el => {
   observer.observe(el);
 });
+// ===== HERO SLIDER =====
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 5000);
